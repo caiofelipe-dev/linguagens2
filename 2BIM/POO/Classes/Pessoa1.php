3d789785ff9fc7aa653e3a618e53d96def7251e3
caiofelipe-dev <caiofelipesmoreira@gmail.com>
2025-04-29 09:01:22 -0300

    Aula 1

---
+++ 2BIM/POO/Classes/Pessoa1.php
@@ -0,0 +1,61 @@
+<?php
+class Pessoa {
+    //Atributos
+    public $nome;
+    public $codigo;
+    public $cpf;
+    public $idade;
+    public $altura;
+    public $salario;
+    
+    //Métodos
+    function inserirDados($nom, $cod, $cpf, $idad, $alt, $sal) {
+        $this->nome = $nom;
+        $this->codigo = $cod;
+        $this->cpf = $cpf;
+        $this->idade = $idad;
+        $this->altura = $alt;
+        $this->salario = $sal;
+    }
+    
+    function mostrar() {
+        echo "
+            <b>Código:</b> $this->codigo<br>
+            <b>CPF: </b> $this->cpf<br>
+            <b>Nome:</b> $this->nome<br>
+            <b>Idade:</b> $this->idade<br>
+            <b>Altura:</b> $this->altura<br>
+            <b>Salário:</b> R$$this->salario<br>
+            <br>
+            ";
+    }
+    
+    function envelhecer($qntd) {
+        if($qntd>0) {
+            $this->idade += $qntd;
+        }
+    }
+    
+    function crescer($cm) {
+        if($cm > 0) {
+            $this->altura += $cm;
+        }
+    }
+    
+    function aumentarSalario($sal) {
+        if($sal > 0) {
+            $this->salario += $sal;
+        }
+    }
+    
+    function dobrarSalario() {
+        $this->salario *= 2;
+    }
+    
+    function alterarCPF() {
+        if($this->cpf > 10) {
+            $this->cpf *= 3;
+        }
+    }
+}
+?>
---
+++ 2BIM/POO/exePessoa1.php
@@ -0,0 +1,36 @@
+    <!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Executável de Pessoa</title>
+    </head>
+    <body>
+        <?php
+        include_once "Classes/pessoa1.php";
+        $p1 = new Pessoa;
+        $p2 = new Pessoa;
+        
+        $p1->inserirDados("Caio Felipe", "001", 9944499900, 18, 1.77, 1500);
+        $p2->inserirDados("Ediondo Carionte", "002", 51561671790, 15, 1.6, 2000);
+        
+        $p1->mostrar();
+        $p2->mostrar();
+        
+        echo '<h4>Valores atualizados</h4>';
+        $p1->envelhecer(10);
+        $p2->envelhecer(20);
+        
+        $p1->crescer(0.1);
+        $p2->crescer(0.05);
+        
+        $p1->dobrarSalario();
+        $p2->dobrarSalario();
+        
+        $p1->alterarCPF();
+        $p2->alterarCPF();
+        
+        $p1->mostrar(); 
+        $p2->mostrar();
+        ?>
+    </body>
+</html>
